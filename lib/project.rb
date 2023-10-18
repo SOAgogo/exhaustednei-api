13d7b5d909bf79bd37597b1333b582497cd505ef
tony <tony6liu@gmail.com>
2023-10-18 15:51:06 +0800

    finish json parser

--- lib/project.rb
+++ lib/project.rb
@@ -1,1 +1,23 @@
-tset
+require 'uri'
+require "net/http"
+require 'pry'
+require 'json'
+require 'yaml'
+# verify your identification
+
+
+begin
+    uri = 'https://data.moa.gov.tw/Service/OpenData/TransService.aspx?UnitId=QcbUEzN6E6DL'
+    url = URI(uri)
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+
+    request = Net::HTTP::Get.new(url)
+    request["accept"] = 'application/json'
+    response = http.request(request)
+
+    body = JSON.parse(response.read_body)
+    puts body
+    #puts body.instance_of? Hash
+end
+
