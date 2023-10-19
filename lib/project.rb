de2595dfdedb19402844de0ae1c5d694738cd7cd
tony <tony6liu@gmail.com>
2023-10-19 21:50:41 +0800

    animals results spec and shelter.rb

--- lib/project.rb
+++ lib/project.rb
@@ -8,16 +8,18 @@ require 'yaml'
 
 begin
     uri = 'https://data.moa.gov.tw/Service/OpenData/TransService.aspx?UnitId=QcbUEzN6E6DL'
+    # params = {'$top'=>'20'}
     url = URI(uri)
+    #url.query = URI.encode_www_form(params)
     http = Net::HTTP.new(url.host, url.port)
     http.use_ssl = true
 
     request = Net::HTTP::Get.new(url)
     request["accept"] = 'application/json'
     response = http.request(request)
-    response.
+    #response.
     body = JSON.parse(response.read_body)
-    puts body[0].keys
+    puts body.size
     #puts body.instance_of? Hash
 end
 
---
+++ spec/spec_helper.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+require 'simplecov'
+SimpleCov.start
+
+require 'json'
+require 'minitest/autorun'
+require 'minitest/unit' # minitest Github issue #17 requires
+require 'minitest/rg'
+require 'vcr'
+require 'webmock'
+
+require_relative '../lib/github_api'
+
+json_file = File.read('./spec/fixtures/DogCat_results.json')
+CORRECT = JSON.parse(json_file)
+
+CASSETTES_FOLDER = 'spec/fixtures/cassettes'
+CASSETTE_FILE = 'animals-record'
