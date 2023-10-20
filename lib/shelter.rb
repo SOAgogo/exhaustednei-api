fa9f0e23addbae8b4cf4fb5132b1725200aec7e4
Justinkao2 <ncy971203@gmail.com>
2023-10-21 00:17:36 +0800

    shlter and animal update

--- lib/shelter.rb
+++ lib/shelter.rb
@@ -14,5 +14,60 @@ module shelter
         def initialize()
         end
     end
-    class Shelter_pkid
-        def initialize()
+
+    
+    class Shelter
+        def Name(name)
+            @name = name
+        end
+        
+        def Shelter_pkid(Shelter_pkid)
+            @Shelter_pkid = Shelter_pkid
+        end
+
+        def Shelter_telephone_number(Shelter_telephone_number)
+            @Shelter_telephone_number = Shelter_telephone_number
+        end
+        
+        def array_animal_ID(animal_ID)
+            #temp
+        end
+        
+        def Shelter_name(Shelter_name)
+            @Shelter_name = Shelter_name
+        end
+        
+        def Shelter_Tel(Shelter_Tel)
+            @Shelter_Tel = Shelter_Tel
+        end
+
+        def Shelter_address(Shelter_address)
+            @Shelter_address = Shelter_address
+        end
+
+    def get_shelter_name(shelter)
+        shelter.Shelter_name
+    end
+
+
+    def query_all_animalid()
+        #temp
+    end
+
+    def get_phone_number(shelter)
+        return shelter.Shelter_telephone_number
+    end
+
+    def get_kind(pet)
+        return pet.animal_kind
+    end
+
+    def get_Variate(pet)
+        return pet.animal_variate
+    end
+    def get_gender(pet)
+        return pet.animal_sex
+    end
+end
+
+
