b929ade97cc50d5322ed5992215580ed81772d8f
tony <tony6liu@gmail.com>
2023-10-22 16:06:14 +0800

    needs to modify project.rb parser

--- lib/project.rb
+++ lib/project.rb
@@ -42,26 +42,42 @@ module Info
       @request_body = JSON.parse(response.read_body)[1..20]
     end
 
-    def animal_parser(data, animal_data_hash = {})
-      data.each do |key, value|
-        animal_data_hash[key] = value unless %w[animal_area_pkid shelter_name shelter_address
-                                                shelter_tel].include?(key)
-      end
-      animal_data_hash
-    end
+    # def animal_parser(data, animal_data_hash = {})
+    #   data.each do |key, value|
+    #     animal_data_hash[key] = value unless %w[animal_area_pkid shelter_name shelter_address
+    #                                             shelter_tel].include?(key)
+    #   end
+    #   animal_data_hash
+    # end
 
-    def shelter_parser(data, shelter_data_hash = {})
+    # def shelter_parser(data, shelter_data_hash = {})
+    #   data.each do |key, value|
+    #     shelter_data_hash[key] = value if %w[animal_area_pkid shelter_name shelter_address shelter_tel].include?(key)
+    #   end
+    #   shelter_data_hash
+    # end
+    def parser(data)
+      animal_data_hash = {}
+      shelter_data_hash = {}
       data.each do |key, value|
-        shelter_data_hash[key] = value if %w[animal_area_pkid shelter_name shelter_address shelter_tel].include?(key)
+        if %w[animal_shelter_pkid animal_id].include?(key)
+          animal_data_hash[key] = value
+          shelter_data_hash[key] = value
+        elsif %w[animal_area_pkid shelter_name shelter_address shelter_tel].include?(key)
+          shelter_data_hash[key] = value
+        else
+          animal_data_hash[key] = value
+        end
       end
-      shelter_data_hash
+      [animal_data_hash, shelter_data_hash]
     end
 
     def initiate_shelterlist
       @shelter_list = ShelterList.new
       @request_body.each do |hash_value|
-        animal_data = animal_parser(hash_value)
-        shelter_data = shelter_parser(hash_value)
+        # animal_data = animal_parser(hash_value)
+        # shelter_data = shelter_parser(hash_value)
+        animal_data, shelter_data = parser(hash_value)
         shelter_initiator(animal_data, shelter_data)
         # @shelter_list = shelter_initiator(@shelter_list, animal_data, shelter_data)
       end
