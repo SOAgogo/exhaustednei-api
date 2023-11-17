b8ae8e9d56b84ec40bafbfa631c8d6f17ea31835
tony <tony6liu@gmail.com>
2023-11-17 16:04:21 +0800

    set cookie expiration time 1y

--- config/environment.rb
+++ config/environment.rb
@@ -21,7 +21,7 @@ module PetAdoption
 
       use Rack::Session::Cookie, {
         secret: config.SESSION_SECRET,
-        expire_after: 30
+        expire_after: 60 * 60 * 24 * 365
       }
       # for testing and development, use sqlite
       configure :development, :test do
