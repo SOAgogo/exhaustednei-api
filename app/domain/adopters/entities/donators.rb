3b61beee430aa84425c582713b4ba41eb3c1c27e
tony <tony6liu@gmail.com>
2023-11-17 23:57:44 +0800

    merge with master

--- app/domain/adopters/entities/donators.rb
+++ app/domain/adopters/entities/donators.rb
@@ -23,17 +23,10 @@ module PetAdoption
         to_hash.except(:address)
       end
 
-<<<<<<< HEAD
-      def pay_money_to_shelter(shelter_pkid, amount)
-        shelter = shelters.find { |shelter| shelter.id == shelter_pkid }
-        shelter.money += amount
-      end
-=======
       # def pay_money_to_shelter(shelter_pkid, amount)
       #   shelter = shelters.find { |shelter| shelter.id == shelter_pkid }
       #   shelter.money += amount
       # end
->>>>>>> master
     end
   end
 end
--- app/domain/adopters/repositories/users.rb
+++ app/domain/adopters/repositories/users.rb
@@ -1,9 +1,6 @@
 # frozen_string_literal: true
 
-<<<<<<< HEAD
-require 'pry'
-=======
->>>>>>> master
+
 module Repository
   # Maps over local and remote git repo infrastructure
   module Adopters
@@ -14,22 +11,6 @@ module Repository
       end
 
       def create_user
-<<<<<<< HEAD
-        Database::ProjectOrm::UserOrm.find_or_create(@user_info)
-      end
-
-      def rebuild_entity
-        return nil unless @user_info
-
-        PetAdoption::Entity::User.new(
-          session_id: @user_info.session_id,
-          firstname: @user_info.firstname,
-          lastname: @user_info.lastname,
-          phone: @user_info.phone,
-          email: @user_info.email,
-          address: @user_info.address,
-          donate_money: @user_info.donate_money
-=======
         user_db = Database::ProjectOrm::UserOrm.find_or_create(@user_info)
         Users.rebuild_entity(user_db)
       end
@@ -45,7 +26,6 @@ module Repository
           email: user_db.email,
           address: user_db.address,
           donate_money: user_db.donate_money
->>>>>>> master
         )
       end
     end
