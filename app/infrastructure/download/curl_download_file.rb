9551aab6295b8decc8c71f5a45b0bdc2129e1d7d
tony <tony6liu@gmail.com>
2023-11-18 22:08:55 +0800

    change the number of elements

--- app/infrastructure/download/curl_download_file.rb
+++ app/infrastructure/download/curl_download_file.rb
@@ -27,7 +27,7 @@ module PetAdoption
           puts 'Error downloading the file.'
         end
         puts 'parse the json file, it may take a while...'
-        JSON.parse(File.read('spec/fixtures/DogCat_results.json'))[0..2000]
+        JSON.parse(File.read('spec/fixtures/DogCat_results.json'))[0..1500]
       end
     end
   end
