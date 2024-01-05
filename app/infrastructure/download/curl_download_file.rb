16e0338ab3597ebe8469cd21c4a37d19b292c193
tony <tony6liu@gmail.com>
2024-01-06 00:01:31 +0800

    run db:datainit

--- app/infrastructure/download/curl_download_file.rb
+++ app/infrastructure/download/curl_download_file.rb
@@ -27,7 +27,7 @@ module PetAdoption
         # else
         #   puts 'Error downloading the file.'
         # end
-        # puts 'parse the json file, it may take a while...'
+        puts 'parse the json file, it may take a while...'
         JSON.parse(File.read('spec/fixtures/DogCat_results.json'))[0..5000]
       end
     end
