f1c5220ff4eb23136c040e504b594b974ecff613
tony <tony6liu@gmail.com>
2024-01-07 02:27:41 +0800

    add shoryuken workers

--- app/infrastructure/download/curl_download_file.rb
+++ app/infrastructure/download/curl_download_file.rb
@@ -28,7 +28,7 @@ module PetAdoption
         #   puts 'Error downloading the file.'
         # end
         puts 'parse the json file, it may take a while...'
-        JSON.parse(File.read('spec/fixtures/DogCat_results.json'))[0..4200]
+        JSON.parse(File.read('spec/fixtures/DogCat_results.json'))[0..3500]
       end
     end
   end
